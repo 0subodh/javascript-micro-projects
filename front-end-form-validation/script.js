1476b34342f47a16e77ba8e1595d030b912e6eff
Subodh Adhikari <subodh1adhikari@gmail.com>
2021-10-07 11:38:17 +0545

    final push

--- front-end-form-validation/script.js
+++ front-end-form-validation/script.js
@@ -4,7 +4,7 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-// show input error message
+// show input error message with error outline
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
@@ -18,33 +18,64 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+// check email is valid or not using regular expressions
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+// get field name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, ` ${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at less than ${max} characters`
+    );
+  }
+}
+
+// check passwords match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
 // event listeners
-
 // submit event fires when the user clicks a submit button in a form, submit event fires on form element itself
-
 // preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else {
-    showSuccess(username);
-  }
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(username);
-  }
-  if (password2.value === "") {
-    showError(password2, "Confirm password is required");
-  } else {
-    showSuccess(username);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 });
